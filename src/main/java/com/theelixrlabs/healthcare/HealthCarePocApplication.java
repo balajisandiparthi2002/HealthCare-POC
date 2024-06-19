4738c5bdc142367a72cd2969dec01681ac4a4120
Balaji <balajisandiparthi@gmail.com>
2024-06-19 13:06:09 +0530

    Created Initial Project

---
+++ src/main/java/com/theelixrlabs/healthcare/HealthCarePocApplication.java
@@ -0,0 +1,13 @@
+package com.theelixrlabs.healthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCarePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCarePocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theelixrlabs/healthcare/HealthCarePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theelixrlabs.healthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCarePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
