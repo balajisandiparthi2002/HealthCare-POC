61de84983bb72e9a3feb699140470301cad8918f
S Chandrakanth <schandrakanth15@gmail.com>
2024-06-26 14:22:29 +0530

    updated with comments and changed the datatype of aadhaarNumber to String

--- src/main/java/com/theelixrlabs/healthcare/constants/DoctorConstants.java
+++ src/main/java/com/theelixrlabs/healthcare/constants/DoctorConstants.java
@@ -4,5 +4,11 @@ package com.theelixrlabs.healthcare.constants;
  * Constants related to doctors in the healthcare system.
  */
 public class DoctorConstants {
-    public static final String DOCTOR_COLLECTION_NAME = "doctors";
+    public static final String DOCTORS_COLLECTION_NAME = "doctors";
+    public static final String FIRST_NAME_SHOULD_BE_MANDATORY = "First name is mandatory";
+    public static final String LAST_NAME_SHOULD_BE_MANDATORY = "Last name is mandatory";
+    public static final String DEPARTMENT_SHOULD_BE_MANDATORY = "Department is mandatory";
+    public static final String AADHAAR_NUMBER_SHOULD_BE_MANDATORY = "Aadhaar number is mandatory";
+    public static final String INVALID_AADHAAR_NUMBER_FORMAT = "Aadhaar number must be exactly 12 digits and contain only numeric characters";
+    public static final String AADHAAR_NUMBER_PATTERN = "^\\d{12}$";
 }
--- src/main/java/com/theelixrlabs/healthcare/model/DoctorsEntity.java
+++ src/main/java/com/theelixrlabs/healthcare/model/DoctorsEntity.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.util.UUID;
 
 /**
@@ -12,14 +14,19 @@ import java.util.UUID;
  * This class is mapped to the MongoDB collection specified by DoctorConstants.DOCTOR_COLLECTION_NAME.
  */
 @Data
-@Document(collection = DoctorConstants.DOCTOR_COLLECTION_NAME)
+@Document(collection = DoctorConstants.DOCTORS_COLLECTION_NAME)
 public class DoctorsEntity {
     @Id
     private UUID id;
+    @NotBlank(message = DoctorConstants.FIRST_NAME_SHOULD_BE_MANDATORY)
     private String firstName;
+    @NotBlank(message = DoctorConstants.LAST_NAME_SHOULD_BE_MANDATORY)
     private String lastName;
+    @NotBlank(message = DoctorConstants.DEPARTMENT_SHOULD_BE_MANDATORY)
     private String department;
-    private long aadhaarNumber;
+    @NotBlank(message = DoctorConstants.AADHAAR_NUMBER_SHOULD_BE_MANDATORY)
+    @Pattern(regexp = DoctorConstants.AADHAAR_NUMBER_PATTERN, message = DoctorConstants.INVALID_AADHAAR_NUMBER_FORMAT)
+    private String aadhaarNumber;
 
     /**
      * Constructor to initialize a new DoctorsEntity object with a randomly generated UUID as ID.
