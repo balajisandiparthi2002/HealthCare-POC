7a17a74cf18c36913ce01de587fcb01f96354af5
Balaji <balajisandiparthi@gmail.com>
2024-07-04 15:49:39 +0530

    Updated Files

--- src/main/java/com/theelixrlabs/healthcare/exceptionHandler/CustomException.java
+++ src/main/java/com/theelixrlabs/healthcare/exceptionHandler/CustomException.java
@@ -1,10 +1,22 @@
 package com.theelixrlabs.healthcare.exceptionHandler;
 
+import org.springframework.context.MessageSource;
+
 /**
  * Custom Exception class for handling exception.
  */
 public class CustomException extends RuntimeException {
     public CustomException(String exceptionMessage) {
         super(exceptionMessage);
+    }
+
+    /**
+     * Constructor to create a CustomException with a message from messages.properties file
+     *
+     * @param messageKey    Key for a corresponding message value in messages.properties files
+     * @param messageSource message source object
+     */
+    public CustomException(String messageKey, MessageSource messageSource) {
+        super(messageSource.getMessage(messageKey, null, null));
     }
 }
