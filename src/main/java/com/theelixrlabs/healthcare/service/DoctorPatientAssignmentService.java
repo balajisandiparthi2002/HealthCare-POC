0dd77003cda7d682c575edae591c11e91e442e1c
SambitKSahu99 <sambitkusahu.99@gmail.com>
2024-07-10 09:49:46 +0530

    updated code as per review comments

--- src/main/java/com/theelixrlabs/healthcare/service/DoctorPatientAssignmentService.java
+++ src/main/java/com/theelixrlabs/healthcare/service/DoctorPatientAssignmentService.java
@@ -8,6 +8,7 @@ import com.theelixrlabs.healthcare.model.DoctorPatientAssignmentModel;
 import com.theelixrlabs.healthcare.repository.DoctorRepository;
 import com.theelixrlabs.healthcare.repository.PatientRepository;
 import com.theelixrlabs.healthcare.utility.MessageUtil;
+import com.theelixrlabs.healthcare.validation.Validator;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -22,13 +23,16 @@ public class DoctorPatientAssignmentService {
     private final PatientRepository patientRepository;
     private final DoctorRepository doctorRepository;
     private final MessageUtil messageUtil;
+    private final Validator validator;
+
 
     public DoctorPatientAssignmentService(DoctorPatientAssignmentRepository doctorPatientAssignmentRepository,
-                                          PatientRepository patientRepository, DoctorRepository doctorRepository, MessageUtil messageUtil) {
+                                          PatientRepository patientRepository, DoctorRepository doctorRepository, MessageUtil messageUtil, Validator validator) {
         this.doctorPatientAssignmentRepository = doctorPatientAssignmentRepository;
         this.patientRepository = patientRepository;
         this.doctorRepository = doctorRepository;
         this.messageUtil = messageUtil;
+        this.validator = validator;
     }
 
     /**
@@ -39,9 +43,11 @@ public class DoctorPatientAssignmentService {
      * @throws CustomException if any exception occurs
      */
     public DoctorPatientAssignmentDto assignDoctorToPatient(DoctorPatientAssignmentDto doctorPatientAssignmentDto) throws CustomException {
-        validateDoctorPatientAssignment(doctorPatientAssignmentDto);
-        UUID doctorId = UUID.fromString(doctorPatientAssignmentDto.getDoctorId());
-        UUID patientId = UUID.fromString(doctorPatientAssignmentDto.getPatientId());
+        UUID doctorId = validator.validateAndConvertToUUID(doctorPatientAssignmentDto.getDoctorId(),
+                DoctorPatientAssignmentConstants.DOCTOR_ID_INVALID_KEY);
+        UUID patientId = validator.validateAndConvertToUUID(doctorPatientAssignmentDto.getPatientId(),
+                DoctorPatientAssignmentConstants.PATIENT_ID_INVALID_KEY);
+        validateDoctorPatientExistence(doctorId, patientId);
         if (isDoctorAlreadyAssigned(doctorId, patientId)) {
             throw new CustomException(messageUtil.getMessage(DoctorPatientAssignmentConstants.DOCTOR_ALREADY_ASSIGNED_KEY));
         }
@@ -55,39 +61,25 @@ public class DoctorPatientAssignmentService {
      * @throws CustomException if any exception occurs
      */
     public void unassignDoctorFromPatient(DoctorPatientAssignmentDto doctorPatientAssignmentDto) throws CustomException {
-        validateDoctorPatientAssignment(doctorPatientAssignmentDto);
-        UUID doctorId = UUID.fromString(doctorPatientAssignmentDto.getDoctorId());
-        UUID patientId = UUID.fromString(doctorPatientAssignmentDto.getPatientId());
+        UUID doctorId = validator.validateAndConvertToUUID(doctorPatientAssignmentDto.getDoctorId(),
+                DoctorPatientAssignmentConstants.DOCTOR_ID_INVALID_KEY);
+        UUID patientId = validator.validateAndConvertToUUID(doctorPatientAssignmentDto.getPatientId(),
+                DoctorPatientAssignmentConstants.PATIENT_ID_INVALID_KEY);
+        validateDoctorPatientExistence(doctorId, patientId);
         Optional<DoctorPatientAssignmentModel> optionalDoctorPatientAssignmentModel =
                 doctorPatientAssignmentRepository.findByDoctorIdAndPatientIdAndDateOfUnassignmentNull(doctorId, patientId);
         if (!doctorPatientAssignmentRepository.existsByDoctorIdAndPatientId(doctorId, patientId)) {
             throw new CustomException(messageUtil.getMessage(DoctorPatientAssignmentConstants.NO_ASSIGNMENT_EXISTS_KEY));
-        } else if (optionalDoctorPatientAssignmentModel.isEmpty()) {
+        }
+        if (optionalDoctorPatientAssignmentModel.isEmpty()) {
             throw new CustomException(messageUtil.getMessage(DoctorPatientAssignmentConstants.DOCTOR_ALREADY_UNASSIGNED_KEY));
         }
         DoctorPatientAssignmentModel activeDoctorPatientAssignmentModel = optionalDoctorPatientAssignmentModel.get();
-        DoctorPatientAssignmentModel doctorPatientAssignmentModel = DoctorPatientAssignmentModel.builder()
-                .id(activeDoctorPatientAssignmentModel.getId())
-                .doctorId(activeDoctorPatientAssignmentModel.getDoctorId())
-                .patientId(activeDoctorPatientAssignmentModel.getPatientId())
-                .dateOfAssignment(activeDoctorPatientAssignmentModel.getDateOfAssignment())
-                .dateOfUnassignment(Date.from(Instant.now()))
-                .build();
-        doctorPatientAssignmentRepository.save(doctorPatientAssignmentModel);
+        activeDoctorPatientAssignmentModel.setDateOfUnassignment(Date.from(Instant.now()));
+        doctorPatientAssignmentRepository.save(activeDoctorPatientAssignmentModel);
     }
 
-    private void validateDoctorPatientAssignment(DoctorPatientAssignmentDto doctorPatientAssignmentDTO) throws CustomException {
-        UUID doctorId, patientId;
-        try {
-            doctorId = UUID.fromString(doctorPatientAssignmentDTO.getDoctorId());
-        } catch (IllegalArgumentException illegalArgumentException) {
-            throw new CustomException(messageUtil.getMessage(DoctorPatientAssignmentConstants.DOCTOR_ID_INVALID_KEY));
-        }
-        try {
-            patientId = UUID.fromString(doctorPatientAssignmentDTO.getPatientId());
-        } catch (IllegalArgumentException illegalArgumentException) {
-            throw new CustomException(messageUtil.getMessage(DoctorPatientAssignmentConstants.PATIENT_ID_INVALID_KEY));
-        }
+    private void validateDoctorPatientExistence(UUID doctorId, UUID patientId) throws CustomException {
         if (!doctorRepository.existsById(doctorId)) {
             throw new CustomException(messageUtil.getMessage(DoctorPatientAssignmentConstants.DOCTOR_NOT_FOUND_KEY));
         }
