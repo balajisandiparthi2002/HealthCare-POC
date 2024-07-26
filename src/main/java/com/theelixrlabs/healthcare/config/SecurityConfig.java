754aac66b4fd3adbab9443f56a228044587f12a1
SambitKSahu99 <sambitkusahu.99@gmail.com>
2024-07-26 12:58:17 +0530

    Resolved comments and deleted ProdSecurityConfig class

--- src/main/java/com/theelixrlabs/healthcare/config/SecurityConfig.java
+++ src/main/java/com/theelixrlabs/healthcare/config/SecurityConfig.java
@@ -5,7 +5,7 @@ import com.theelixrlabs.healthcare.utility.AuthenticationEntryPointUtility;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -19,20 +19,21 @@ import static org.springframework.security.config.Customizer.withDefaults;
  */
 @Configuration
 @EnableWebSecurity
-@Profile(SecurityConstants.DEV_PROFILE_CONSTANT)
 public class SecurityConfig {
 
-
     private final String JWK_SET_URI;
+    private final Environment environment;
     private final AuthenticationEntryPointUtility authenticationEntryPointUtility;
 
-    public SecurityConfig(@Value(SecurityConstants.GOOGLE_JWK_SET_URI) String jwkSetUri, AuthenticationEntryPointUtility authenticationEntryPointUtility) {
+    public SecurityConfig(@Value(SecurityConstants.GOOGLE_JWK_SET_URI) String jwkSetUri,
+                          Environment environment, AuthenticationEntryPointUtility authenticationEntryPointUtility) {
         JWK_SET_URI = jwkSetUri;
+        this.environment = environment;
         this.authenticationEntryPointUtility = authenticationEntryPointUtility;
     }
 
     /**
-     * This bean configures spring security with Google Authentication
+     * This bean configures spring security with Google Authentication based on profiles
      *
      * @param httpSecurity HttpSecurity instance
      * @return SecurityFilterChain instance
@@ -40,14 +41,21 @@ public class SecurityConfig {
      */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        String activeProfile = environment.getProperty(SecurityConstants.ACTIVE_PROFILE);
+        if (!activeProfile.equals(SecurityConstants.PROD_PROFILE_CONSTANT)) {
+            httpSecurity
+                    .authorizeHttpRequests(authorize -> authorize
+                            .anyRequest().authenticated())
+                    .oauth2Login(withDefaults())
+                    .oauth2Client(withDefaults())
+                    .oauth2ResourceServer(oauth2 -> oauth2
+                            .jwt(withDefaults())
+                            .authenticationEntryPoint(authenticationEntryPointUtility));
+            return httpSecurity.build();
+        }
         httpSecurity
                 .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().authenticated())
-                .oauth2Login(withDefaults())
-                .oauth2Client(withDefaults())
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(withDefaults())
-                        .authenticationEntryPoint(authenticationEntryPointUtility));
+                        .anyRequest().permitAll());
         return httpSecurity.build();
     }
 
--- src/main/java/com/theelixrlabs/healthcare/constants/SecurityConstants.java
+++ src/main/java/com/theelixrlabs/healthcare/constants/SecurityConstants.java
@@ -4,7 +4,7 @@ package com.theelixrlabs.healthcare.constants;
  * Constants for spring security implementation
  */
 public class SecurityConstants {
-    public static final String DEV_PROFILE_CONSTANT = "dev";
+    public static final String ACTIVE_PROFILE = "spring.profiles.active";
     public static final String GOOGLE_JWK_SET_URI = "${spring.security.oauth2.client.provider.google.jwk-set-uri}";
     public static final String PROD_PROFILE_CONSTANT = "prod";
 }
