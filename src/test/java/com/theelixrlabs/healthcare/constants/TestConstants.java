fd1eba26c84e00c58ea84692ec19f31db8ebcfa7
S Chandrakanth <schandrakanth15@gmail.com>
2024-07-30 15:33:05 +0530

    Added positive and negative test cases for the endpoint 'get doctor by name'

---
+++ src/test/java/com/theelixrlabs/healthcare/constants/TestConstants.java
@@ -0,0 +1,7 @@
+package com.theelixrlabs.healthcare.constants;
+
+public class TestConstants {
+    public static final String DOCTOR_NAME = "ABCDEFGH";
+    public static final String DOCTOR_NAME_NOT_FOUND = "doctor.name.not.found";
+
+}
