af8471b382d9fd9335eeca23f9dacda073004dec
Balaji <balajisandiparthi@gmail.com>
2024-07-30 10:40:38 +0530

    Updated Constants

--- src/test/java/com/theelixrlabs/healthcare/constants/TestConstants.java
+++ src/test/java/com/theelixrlabs/healthcare/constants/TestConstants.java
@@ -3,11 +3,11 @@ package com.theelixrlabs.healthcare.constants;
 public class TestConstants {
     public static final String DELETION_FAILED = "Deletion Failed. Patient is currently assigned to a Doctor.";
     public static final String ERRORS_ARRAY_EXPRESSION = "$.errors";
-    public static final String TEST_RESULT_ARRAY_EXPRESSION = "$.errors[0]";
     public static final String PATIENT_BY_ID = "/patient/{patientId}";
     public static final String PATIENT_DELETED_SUCCESSFULLY = "Patient deleted successfully";
     public static final String PATIENT_ID = "1a3d5906-fa50-4f2e-b02c-5ebc76ccfb30";
     public static final String PATIENT_NOT_FOUND = "Patient not found.";
     public static final String RESPONSE_MESSAGE_EXPRESSION = "$.responseMessage";
     public static final String SUCCESS_EXPRESSION = "$.success";
+    public static final String TEST_RESULT_ARRAY_EXPRESSION = "$.errors[0]";
 }
