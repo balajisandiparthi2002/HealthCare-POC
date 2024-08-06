5a20dc30f0325eacb95b50b072f236012dadec62
Shahana2601 <shahanalfajar@gmail.com>
2024-08-06 10:19:04 +0530

    resolved merge conflicts

--- src/test/java/com/theelixrlabs/healthcare/constants/TestConstants.java
+++ src/test/java/com/theelixrlabs/healthcare/constants/TestConstants.java
@@ -1,13 +1,13 @@
 package com.theelixrlabs.healthcare.constants;
 
 /**
- * This class holds constant values used for testing purposes in the healthcare application.
+ * Constant class containing error messages and endpoints of doctor.
  */
 public class TestConstants {
     public static final String DOCTOR_ENDPOINT = "/doctor/";
-    public static final String DOCTOR_NOT_FOUND_MESSAGE = "doctor not found";
-    public static final String INVALID_UUID = "Invalid uuid";
     public static final String DOCTOR_NAME = "ABCDEFGH";
     public static final String DOCTOR_NAME_NOT_FOUND = "doctor.name.not.found";
+    public static final String DOCTOR_NOT_FOUND_MESSAGE = "doctor not found";
+    public static final String INVALID_UUID = "Invalid uuid";
     public static final String PATIENT_NOT_FOUND_MESSAGE = "Patient not found";
 }
